0c08081e9a1ddb9107eea1f2c10ede38998e1a5f
Manuel <mimunoz11@uc.cl>
2024-11-16 22:45:12 -0300

    clean code

--- src/middleware.ts
+++ src/middleware.ts
@@ -9,16 +9,13 @@ export async function middleware(request: NextRequest) {
     data: { user },
   } = await supabase.auth.getUser()
 
-  // Si el usuario no está autenticado y está intentando acceder a una ruta protegida
   if (!user && request.nextUrl.pathname.startsWith('/members')) {
-    // Redirige al usuario a la página de inicio de sesión
     return NextResponse.redirect(new URL('/login', request.url))
   }
 
-  // Permite el acceso a la ruta
   return response
 }
 
 export const config = {
-  matcher: ['/members/:path*'], // Protege todas las rutas bajo /members
+  matcher: ['/members/:path*'],
 }
