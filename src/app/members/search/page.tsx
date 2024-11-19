602b5d58617a67d40677e35707bfb05f94db5be1
gustavopalaciosc <gpalaciosc@uc.cl>
2024-11-19 15:42:21 -0300

    refactor(kpi-summary): Move Supabase request from client to server for improved security

--- src/app/members/search/page.tsx
+++ src/app/members/search/page.tsx
@@ -2,27 +2,26 @@
 import { useState, useEffect } from 'react'
 import SearchFilterButton from '@/components/ui/SearchFilterButton'
 import SearchBar from '@/components/ui/SearchBar'
-import { createBrowserClient } from '@/utils/supabase'
 import Navbar from '@/components/ui/Navbar/Navbar'
 
 export default function MemberSearch() {
   const [selectedButton, setSelectedButton] = useState<string>('registered')
-  const supabase = createBrowserClient()
   const [registered, setRegistered] = useState<number>(0)
   const [accessed, setAccesed] = useState<number>(0)
   const [pendingAccess, setPendingAccess] = useState<number>(0)
 
   async function fetchData() {
-    const { data, error } = await supabase.rpc('get_kpi_summary')
-    if (error) {
-      console.error(error)
-    } else {
-      console.log(data)
-      if (data) {
-        setRegistered(data.registered || 0)
-        setAccesed(data.accessed || 0)
-        setPendingAccess(data.pending_access || 0)
+    try {
+      const response = await fetch('/api/kpi-summary')
+      if (!response.ok) {
+        throw new Error('Error fetching data')
       }
+      const data = await response.json()
+      setRegistered(data.registered || 0)
+      setAccesed(data.accessed || 0)
+      setPendingAccess(data.pending_access || 0)
+    } catch (error) {
+      console.error('Error fetching data:', error)
     }
   }
 
@@ -35,36 +34,34 @@ export default function MemberSearch() {
   }
 
   return (
-    <>
-      <div className="mx-auto flex max-w-3xl flex-col items-center justify-center space-y-4">
-        <Navbar />
-        <div className="flex flex-col items-center space-y-4">
-          <div className="flex w-full justify-center space-x-8">
-            <SearchFilterButton
-              label="registrados"
-              count={registered}
-              isSelected={selectedButton === 'registered'}
-              onClick={() => handleButtonClick('registered')}
-            />
-            <SearchFilterButton
-              label="accedidos"
-              count={accessed}
-              isSelected={selectedButton === 'accessed'}
-              onClick={() => handleButtonClick('accessed')}
-            />
-            <SearchFilterButton
-              label="por acceder"
-              count={pendingAccess}
-              isSelected={selectedButton === 'pending_access'}
-              onClick={() => handleButtonClick('pending_access')}
-            />
-          </div>
+    <div className="mx-auto flex max-w-3xl flex-col items-center justify-center space-y-4">
+      <Navbar />
+      <div className="flex flex-col items-center space-y-4">
+        <div className="flex w-full justify-center space-x-8">
+          <SearchFilterButton
+            label="registrados"
+            count={registered}
+            isSelected={selectedButton === 'registered'}
+            onClick={() => handleButtonClick('registered')}
+          />
+          <SearchFilterButton
+            label="accedidos"
+            count={accessed}
+            isSelected={selectedButton === 'accessed'}
+            onClick={() => handleButtonClick('accessed')}
+          />
+          <SearchFilterButton
+            label="por acceder"
+            count={pendingAccess}
+            isSelected={selectedButton === 'pending_access'}
+            onClick={() => handleButtonClick('pending_access')}
+          />
+        </div>
 
-          <div className="flex w-full justify-center">
-            <SearchBar selectedButton={selectedButton} />
-          </div>
+        <div className="flex w-full justify-center">
+          <SearchBar selectedButton={selectedButton} />
         </div>
       </div>
-    </>
+    </div>
   )
 }
