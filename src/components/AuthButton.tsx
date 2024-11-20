0eb6f3ce011e5526b14d4b976b0b91f81cc8034a
gustavopalaciosc <gpalaciosc@uc.cl>
2024-11-20 12:00:00 -0300

    fix(google-auth): made check-session server side.

--- src/components/AuthButton.tsx
+++ src/components/AuthButton.tsx
@@ -1,24 +1,61 @@
 'use client'
 
 import { useEffect, useState } from 'react'
-import { redirect } from 'next/navigation'
+import { useRouter } from 'next/navigation'
 import { createBrowserClient } from '@/utils/supabase'
+import { User } from '@supabase/supabase-js' // Import the User type
 
 const supabase = createBrowserClient()
 
 export default function AuthButton() {
-  const [user, setUser] = useState(null)
+  const [user, setUser] = useState<User | null>(null) // Define the state type
+  const [loading, setLoading] = useState<boolean>(true)
+  const router = useRouter()
 
   useEffect(() => {
     const getUser = async () => {
-      const {
-        data: { user },
-      } = await supabase.auth.getUser()
-      setUser(user)
+      // Call the endpoint to check the session
+      try {
+        const response = await fetch('/api/auth/check-session')
+        const data = await response.json()
+
+        // If the session is active, set the user
+        if (data.user) {
+          setUser(data.user)
+        } else {
+          setUser(null)
+        }
+      } catch (error) {
+        console.log(error)
+      } finally {
+        setLoading(false)
+      }
     }
 
     getUser()
   }, [])
+
+  useEffect(() => {
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange((_, session) => {
+      if (session?.user) {
+        setUser(session.user)
+      } else {
+        setUser(null)
+      }
+    })
+
+    return () => {
+      subscription.unsubscribe()
+    }
+  }, [])
+
+  useEffect(() => {
+    if (user) {
+      router.push('/members/search')
+    }
+  }, [user, router])
 
   const signInWithGoogle = async () => {
     const { error } = await supabase.auth.signInWithOAuth({
@@ -33,13 +70,15 @@ export default function AuthButton() {
     }
   }
 
-  return user ? (
-    redirect('/members/search')
-  ) : (
+  if (loading) {
+    return <div>Loading...</div>
+  }
+
+  return (
     <div className="flex items-center gap-4">
       <button
         onClick={signInWithGoogle}
-        className="bg-btn-background hover:bg-btn-background-hover rounded-md px-4 py-2 no-underline"
+        className="rounded-[10px] bg-[#FFEC40] p-2 px-8 text-[14px] text-black hover:bg-[#F9BC12]"
       >
         Iniciar Sesión
       </button>
